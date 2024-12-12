30082a7aee273b55c85dd2ed8bab0464712dadfc
Keigo Murata <85227053+k5mrt@users.noreply.github.com>
2024-12-12 16:06:52 +0900

    unneed

--- src/App.vue
+++ src/App.vue
@@ -14,17 +14,6 @@ const selectFile = async () => {
 </script>
 
 <template>
-  <div>
-    <a href="https://www.electronjs.org/" target="_blank">
-      <img src="./assets/electron.svg" class="logo electron" alt="Electron logo" />
-    </a>
-    <a href="https://vitejs.dev/" target="_blank">
-      <img src="./assets/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
   <HelloWorld msg="Electron + Vite + Vue" />
   <div class="flex-center">
     Place static files into the <code>/public</code> folder
@@ -36,29 +25,10 @@ const selectFile = async () => {
   </div>
 </template>
 
-<style>
+<style lang="scss">
 .flex-center {
   display: flex;
   align-items: center;
   justify-content: center;
-}
-
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo.electron:hover {
-  filter: drop-shadow(0 0 2em #9FEAF9);
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
